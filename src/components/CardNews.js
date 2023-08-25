556fbcdb6396329f4c080524c5effe22232e7beb
marlupatrocinio <marlupatrocinio01@gmail.com>
2023-08-25 11:02:59 -0300

    Code optimization

--- src/components/CardNews.js
+++ src/components/CardNews.js
@@ -22,7 +22,11 @@ class CardNews extends HTMLElement {
         autor.textContent = "By: " + (this.getAttribute("autor") || "quem mais? Ronaldo, claro");
 
         const linkTitle = document.createElement("a");
+        linkTitle.textContent = this.getAttribute("title");
+        linkTitle.href = this.getAttribute("link-url");
+
         const newsContent = document.createElement("p");
+        newsContent.textContent = this.getAttribute("content");
 
         cardLeft.appendChild(autor);
         cardLeft.appendChild(linkTitle);
@@ -32,13 +36,51 @@ class CardNews extends HTMLElement {
         cardRight.setAttribute("class", "cardRight");
 
         const newsImage = document.createElement("img");
-
+        newsImage.src = this.getAttribute("photo") || "/assets/02.jpeg";
+        newsImage.alt = "Foto do ronaldinho";
         cardRight.appendChild(newsImage);
         
         return componentRoot;
     }
     styles() {
+        const style = document.createElement("style");
+        style.textContent = `
+        .card {
+            font-family: 'Segoe UI', sans-serif;
+            width: 720px;
+            border: 1px solid gray;
+            display: flex;
+            flex-direction: row;
+            -webkit-box-shadow: 11px 11px 32px -3px rgba(0,0,0,0.58);
+            -moz-box-shadow: 11px 11px 32px -3px rgba(0,0,0,0.58);
+            box-shadow: 11px 11px 32px -3px rgba(0,0,0,0.58);
+            justify-content: space-between;
+        }
+        
+        .cardLeft {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            padding-left: 10px;
+        }
+        
+        .cardLeft > span {
+            font-weight: 400;
+        }
+        
+        .cardLeft > a {
+            margin-top: 15px;
+            font-size: 25px;
+            color: black;
+            text-decoration: none;
+            font-weight: bold;
+        }
+        .cardLeft > p {
+            color: gray;
+        }
+        `;
 
+        return style;
     }
 }
 
